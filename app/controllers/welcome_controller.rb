9a87f6801f0c15cf036bec41cebfdf7c90c11316
simontaosim <xsqfeather@126.com>
2015-03-28 01:49:03 +0800

    123

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,16 @@
 class WelcomeController < ApplicationController
   #layout 'purestyle'
+  before_action :get_user_agent
   def index
-  	
+  	if session[:user_agent].include?('Android') || session[:user_agent].include?('iPhone')
+  		respond_to do |format|
+		    format.html { redirect_to mobile_view_index_path, notice: 'Session was successfully updated.' }
+    	end
+  	end
+  end
+
+  private
+  def get_user_agent
+  	session[:user_agent] ||= request.user_agent
   end
 end

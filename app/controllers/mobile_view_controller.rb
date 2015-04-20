82b4fe849ed5ad8b1cc61fa22f27963671996062
simontaosim <xsqfeather@126.com>
2015-04-20 11:26:14 +0800

    123

--- app/controllers/mobile_view_controller.rb
+++ app/controllers/mobile_view_controller.rb
@@ -27,16 +27,17 @@ class MobileViewController < ApplicationController
   end
 
   def show_product
+    param_product_name = "奔跑吧，兔子"
     if params[:product_id]
       @product = Product.find(params[:product_id])
     end
     if session[:from_index]
-      param_product_name = "奔跑吧，兔子"
-      @product = Product.where(:name => param_product_name ).first
+      
+      @product = Product.where(:name => /#param_product_name/ ).first
     end
 
     if session[:username]
-       @product = Product.where(:name => param_product_name ).first
+       @product = Product.where(:name => /#param_product_name/ ).first
     else
       # respond_to do |format|
       #   format.html { redirect_to mobile_view_login_path }
